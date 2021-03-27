16d39f990ab5667abc7f74120bedc9c8edfc87f9
Jahid101 <jahidhasananik.official@gmail.com>
2021-03-27 15:10:25 +0600

    lets go

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBozEmkClNS6S6Wbhro5UY25InzofrrCB0",
+  authDomain: "burj-al-arab-3fec5.firebaseapp.com",
+  projectId: "burj-al-arab-3fec5",
+  storageBucket: "burj-al-arab-3fec5.appspot.com",
+  messagingSenderId: "711087945119",
+  appId: "1:711087945119:web:57ca006a348dfeffe7f9aa"
+};
+export default firebaseConfig;
